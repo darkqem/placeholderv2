350e0e93a8895c45b5f82af0db148502ebac91ac
MSWeiz <134125504+MSWeiz@users.noreply.github.com>
2025-11-02 18:27:58 +0500

    Добавил механику взаимодействия

--- My project/Assets/Scripts/Inventory/Item.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Item : MonoBehaviour
-{
-    public string itemName;
-    public Sprite itemIcon;
-    // Дополнительные свойства предмета, такие как описание или эффект, можно добавить по необходимости.
-}
